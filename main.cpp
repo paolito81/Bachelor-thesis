32a7b512f6b472904ec5d18259a1c01ec1f1ca82
paolito81 <pusterlapaolo81@gmail.com>
2024-05-01 21:14:44 +0200

    Fuck this shit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <display.cpp>
 #include <string>
+#include <TF1.h>
+#include <TH1.h>
 
 int main (){
-    std::cout << "Hello world!" << std::endl;
-    
     display("welcome");
-    
+
+    TF1* gauss_background = new TF1("gauss background", "[0]*x + [1]", 0, 20);
+    TH1* histogram = new TH1("histo", "histogram", 10, 0, 100);
+
     return 0;
 }
